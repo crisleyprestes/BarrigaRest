a1bb64c6833c890096d0d01d9aa8a14edfe34681
Crisley Linhares <prestes40@gmail.com>
2023-03-26 12:38:58 -0400

    Accounts test

--- src/main/java/br/com/linhares/crisley/tests/refact/ContasTest.java
+++ src/main/java/br/com/linhares/crisley/tests/refact/ContasTest.java
@@ -1,13 +1,20 @@
 package br.com.linhares.crisley.tests.refact;
 
+import br.com.linhares.crisley.rest.core.BaseTest;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.is;
 
-public class ContasTest {
+public class ContasTest extends BaseTest {
+
+    public Integer getIdContaPeloNome(String nomeConta){
+        return get("/contas?nome=" + nomeConta).then().extract().path("id[0]");
+    }
 
     @BeforeClass
     public static void login(){
@@ -27,5 +34,43 @@ public class ContasTest {
         requestSpecification.header("Authorization", "JWT " + TOKEN);
 
         get("/reset").then().statusCode(200);
+    }
+
+    @Test
+    public void deveIncluirContaComSucesso(){
+        given()
+                    .body("{\"nome\": \"Conta inserida\"}")
+                .when()
+                    .post("/contas")
+                .then()
+                    .statusCode(201)
+        ;
+    }
+
+    @Test
+    public void deveAlterarContaComSucesso(){
+        Integer CONTA_ID = getIdContaPeloNome("Conta para alterar");
+
+        given()
+                    .body("{\"nome\": \"Conta alterada\"}")
+                    .pathParam("id", CONTA_ID)
+                .when()
+                    .put("/contas/{id}")
+                .then()
+                    .statusCode(200)
+                    .body("nome", is("Conta alterada"))
+        ;
+    }
+
+    @Test
+    public void naoDeveIncluirContaComMesmoNome(){
+        given()
+                    .body("{\"nome\": \"Conta mesmo nome\"}")
+                .when()
+                    .post("/contas")
+                .then()
+                    .statusCode(400)
+                    .body("error", is("Já existe uma conta com esse nome!"))
+        ;
     }
 }
