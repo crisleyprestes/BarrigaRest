9a417d80431c2747206e2a724ab0dc3370a880a2
Crisley Linhares <prestes40@gmail.com>
2023-03-25 22:12:11 -0400

    Should add an account successfully

--- src/main/java/br/com/linhares/crisley/tests/BarrigaTest.java
+++ src/main/java/br/com/linhares/crisley/tests/BarrigaTest.java
@@ -3,6 +3,9 @@ package br.com.linhares.crisley.tests;
 import br.com.linhares.crisley.rest.core.BaseTest;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 
 public class BarrigaTest extends BaseTest{
@@ -16,4 +19,30 @@ public class BarrigaTest extends BaseTest{
                     .statusCode(401)
         ;
     }
+
+    @Test
+    public void deveIncluirContaComSucesso(){
+        Map<String, String> login = new HashMap<>();
+        login.put("email", "crisley@email.com");
+        login.put("senha", "123456");
+
+        String token = given()
+                    .body(login)
+                .when()
+                    .post("/signin")
+                .then()
+                    .statusCode(200)
+                    .extract().path("token")
+        ;
+
+        given()
+                    .header("Authorization", "JWT " + token)
+                    .body("{\"nome\": \"conta qualquer\"}")
+                .when()
+                    .post("/contas")
+                .then()
+                    .statusCode(201)
+        ;
+    }
 }
+
