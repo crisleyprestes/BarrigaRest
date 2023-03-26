4815efda49c37f8b16fdde8392291d4ba5b22db9
Crisley Linhares <prestes40@gmail.com>
2023-03-25 22:19:39 -0400

    Should edit an account successfully

--- src/main/java/br/com/linhares/crisley/tests/BarrigaTest.java
+++ src/main/java/br/com/linhares/crisley/tests/BarrigaTest.java
@@ -1,14 +1,35 @@
 package br.com.linhares.crisley.tests;
 
 import br.com.linhares.crisley.rest.core.BaseTest;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class BarrigaTest extends BaseTest{
+
+    private String TOKEN;
+
+    @Before
+    public void login(){
+        Map<String, String> login = new HashMap<>();
+        login.put("email", "crisley@email.com");
+        login.put("senha", "123456");
+
+        TOKEN = given()
+                .body(login)
+                .when()
+                .post("/signin")
+                .then()
+                .statusCode(200)
+                .extract().path("token")
+                ;
+    }
 
     @Test
     public void naoDeveAcessarAPISemToken(){
@@ -22,26 +43,26 @@ public class BarrigaTest extends BaseTest{
 
     @Test
     public void deveIncluirContaComSucesso(){
-        Map<String, String> login = new HashMap<>();
-        login.put("email", "crisley@email.com");
-        login.put("senha", "123456");
-
-        String token = given()
-                    .body(login)
-                .when()
-                    .post("/signin")
-                .then()
-                    .statusCode(200)
-                    .extract().path("token")
-        ;
-
         given()
-                    .header("Authorization", "JWT " + token)
+                    .header("Authorization", "JWT " + TOKEN)
                     .body("{\"nome\": \"conta qualquer\"}")
                 .when()
                     .post("/contas")
                 .then()
                     .statusCode(201)
+        ;
+    }
+
+    @Test
+    public void deveAlterarContaComSucesso(){
+        given()
+                    .header("Authorization", "JWT " + TOKEN)
+                    .body("{\"nome\": \"conta alterada\"}")
+                .when()
+                    .put("/contas/1670383")
+                .then()
+                    .statusCode(200)
+                    .body("nome", is("conta alterada"))
         ;
     }
 }
